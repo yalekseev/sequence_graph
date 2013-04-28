0513add72317a6afdc9847372ff32e7f09a561eb
Yury Alekseev <yury_alekseev@yahoo.com>
2013-04-29 00:14:37 +0400

    Add unit tests 5 and 6.

--- tests.cpp
+++ tests.cpp
@@ -51,5 +51,28 @@ int main() {
     std::cout << "TEST 4: FAILED" << std::endl;
   }
 
+  sequence_graph.clear();
+
+  sequence_graph.add_sequence("aa");
+
+  if (sequence_graph.has_cycle()) {
+    std::cout << "TEST 5: PASSED" << std::endl;
+  } else {
+    std::cout << "TEST 5: FAILED" << std::endl;
+  }
+
+  sequence_graph.clear();
+
+  sequence_graph.add_sequence("abdc");
+  sequence_graph.add_sequence("abc");
+  sequence_graph.add_sequence("ac");
+  sequence_graph.add_sequence("ad");
+
+  if (!sequence_graph.has_cycle()) {
+    std::cout << "TEST 6: PASSED" << std::endl;
+  } else {
+    std::cout << "TEST 6: FAILED" << std::endl;
+  }
+
   return 0;
 }
